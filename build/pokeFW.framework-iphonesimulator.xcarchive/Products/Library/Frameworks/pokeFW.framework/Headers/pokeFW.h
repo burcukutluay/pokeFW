7bb300052f016fb2544b3fedcf9472280155bc77
Burcu Kutluay <burcukirik@gmail.com>
2021-02-15 01:00:04 +0300

    -- xcframework created. -- pods changed. -- project settings for XCFramework configured.

---
+++ build/pokeFW.framework-iphonesimulator.xcarchive/Products/Library/Frameworks/pokeFW.framework/Headers/pokeFW.h
@@ -0,0 +1,18 @@
+//
+//  pokeFW.h
+//  pokeFW
+//
+//  Created by burcu kirik on 6.02.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for pokeFW.
+FOUNDATION_EXPORT double pokeFWVersionNumber;
+
+//! Project version string for pokeFW.
+FOUNDATION_EXPORT const unsigned char pokeFWVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <pokeFW/PublicHeader.h>
+
+
